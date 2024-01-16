1b000c09b12e075ff8eb4f51b3934e388772fbdc
jum0624 <jum0624@naver.com>
2024-01-16 14:04:03 +0900

    refactor: add Action gradle

--- src/test/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplicationTests.java
+++ src/test/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplicationTests.java
@@ -1,13 +1,13 @@
-//package me.dangtong.springbootdeveloper;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SpringbootdeveloperApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package me.dangtong.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
